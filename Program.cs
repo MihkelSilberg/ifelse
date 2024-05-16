0c985aca8a6c9198082af0fb350ecd899ac629dc
opilane <opilane@HP-CZC0359NN7>
2024-05-16 09:12:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace woolomoolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int x, y;
+
+            x = 3;
+            y = 1;
+            if (y > x)
+            {
+                Console.WriteLine("y on suurem kui x");
+            }
+            else
+            {
+                Console.WriteLine(" x on suurem kui y");
+            }
+        }
+    }
+}
